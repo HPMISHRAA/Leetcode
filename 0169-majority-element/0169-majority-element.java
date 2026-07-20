4be866d1f70bc8bcc9d1763b4c2463528464fe96
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-07-20 19:13:46 +0530

    Time: 16 ms (20.64%), Space: 52.8 MB (73.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,24 +1,33 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int count=0,elem=0,n=nums.length;
-        for(int i=0;i<n;i++){
-            if(count==0){
-                count++;
-                elem=nums[i];
-            }else if(nums[i]==elem){
-                count++;
-            }else{
-                count--;
+        // int count=0,elem=0,n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     if(count==0){
+        //         count++;
+        //         elem=nums[i];
+        //     }else if(nums[i]==elem){
+        //         count++;
+        //     }else{
+        //         count--;
+        //     }
+        // }
+        // int count1=0;
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==elem){
+        //         count1++;
+        //     }
+        // }
+        // if(count1>(n/2)){
+        //     return elem;
+        // }
+        // return -1;
+        // ----------------------  Using HashMap
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int val : nums) {
+            hm.put(val, hm.getOrDefault(val, 0) + 1);
+            if (hm.get(val) > nums.length / 2) {
+                return val;
             }
-        }
-        int count1=0;
-        for(int i=0;i<n;i++){
-            if(nums[i]==elem){
-                count1++;
-            }
-        }
-        if(count1>(n/2)){
-            return elem;
         }
         return -1;
     }
