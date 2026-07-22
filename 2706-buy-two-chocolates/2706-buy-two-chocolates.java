c3ef81b93ca7aae31feaf2ddf2aa8152d28ad1b7
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-07-22 17:58:02 +0530

    Time: 1 ms (100%), Space: 45.8 MB (99.44%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.java
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.java
@@ -1,7 +1,25 @@
 class Solution {
     public int buyChoco(int[] prices, int money) {
-        Arrays.sort(prices);
-        int buy=prices[0]+prices[1];
+        // Brute Force---------
+        // Arrays.sort(prices);
+        // int buy=prices[0]+prices[1];
+        // if(buy>money){
+        //     return money;
+        // }else{
+        //     return money-buy;
+        // }
+
+        int fm=Integer.MAX_VALUE;
+        int sm=Integer.MAX_VALUE;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i]<fm){
+                sm=fm;
+                fm=prices[i];
+            }else if(prices[i]>=fm && prices[i]<=sm){
+                sm=prices[i];
+            }
+        }
+        int buy=fm+sm;
         if(buy>money){
             return money;
         }else{
