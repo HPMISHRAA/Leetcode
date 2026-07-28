2d9420a9868eaf7e82cd2006ebafdf4da54536d8
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-07-28 13:41:19 +0530

    Time: 1 ms (4.39%), Space: 44.3 MB (31.14%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -10,26 +10,35 @@
  */
 class Solution {
     public ListNode reverseList(ListNode head) {
-        if(head==null || head.next==null){
+        if (head == null || head.next == null) {
             return head;
         }
         ListNode temp=head;
-        // ArrayList<Integer> arr=new ArrayList<>();
-        Stack<Integer> sc=new Stack<>();
-        while(temp!=null){
-            // arr.add(temp.val);
-            sc.push(temp.val);
-            temp=temp.next;
-        }
-        temp=head;
-        while(!sc.isEmpty()){
-            temp.val=sc.pop();
-            temp=temp.next;
-        }
-        // for(int i=arr.size()-1;i>=0;i--){
-        //     temp.val=arr.get(i);
+        // Using Stack
+        // Stack<Integer> sc=new Stack<>();
+        // while(temp!=null){
+        //     // arr.add(temp.val);
+        //     sc.push(temp.val);
         //     temp=temp.next;
         // }
+        // temp=head;
+        // while(!sc.isEmpty()){
+        //     temp.val=sc.pop();
+        //     temp=temp.next;
+        // }
+
+        // -------using ArrayList
+        ArrayList<Integer> arr = new ArrayList<>();
+        while (temp != null) {
+            arr.add(temp.val);
+            temp = temp.next;
+        }
+        temp = head;
+
+        for (int i = arr.size() - 1; i >= 0; i--) {
+            temp.val = arr.get(i);
+            temp = temp.next;
+        }
         return head;
     }
 }
