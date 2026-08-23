08aa1875e481e66f6c09378d13be92a1b36fa7b5
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-08-23 12:43:27 +0530

    Time: 5 ms (42.46%), Space: 96.3 MB (32.37%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,6 +10,7 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+        /*
         ListNode temp = head;
         Stack<Integer> sc = new Stack<>();
         while (temp != null) {
@@ -24,5 +25,42 @@ class Solution {
             temp = temp.next;
         }
         return true;
+        */
+
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode newHead = reverseList(slow.next);
+        ListNode first = head;
+        ListNode second = newHead;
+        while (second != null) {
+            if (first.val != second.val) {
+                reverseList(newHead);
+                return false;
+            }
+            first=first.next;
+            second=second.next;
+        }
+        reverseList(newHead);
+        return true;
+
+    }
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
     }
 }
