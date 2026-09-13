cab1f044ae3587d633ec8ac8ca665a12ab0b1d7d
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-09-13 14:27:18 +0530

    Time: 2 ms (59.67%), Space: 43.3 MB (20.59%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -4,17 +4,17 @@ class Solution {
         Stack<Character> second = new Stack<>();
         for (int i = 0; i < s.length(); i++) {
             char ch = s.charAt(i);
-            if(ch!='#'){
+            if (ch != '#') {
                 first.push(ch);
-            }else if(!first.isEmpty()){
+            } else if (!first.isEmpty()) {
                 first.pop();
             }
         }
         for (int i = 0; i < t.length(); i++) {
             char ch = t.charAt(i);
-            if(ch!='#'){
+            if (ch != '#') {
                 second.push(ch);
-            }else if(!second.isEmpty()){
+            } else if (!second.isEmpty()) {
                 second.pop();
             }
         }
