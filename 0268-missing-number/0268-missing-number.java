4df7a2c69d950140adbefb75f4cd73968f3b36ca
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-09-15 22:40:28 +0530

    Time: 0 ms (100%), Space: 47.2 MB (65.99%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,5 +1,6 @@
 class Solution {
     public int missingNumber(int[] nums) {
+        /*
         int n = nums.length;
         int sum = n * (n + 1) / 2;
         int sum1 = 0;
@@ -7,5 +8,14 @@ class Solution {
             sum1 += val;
         }
         return (sum - sum1);
+        */
+
+        int i=1;
+        int xor=0;
+        for(int j=0;j<nums.length;j++){
+            xor^=i^nums[j];
+            i++;
+        }
+        return xor;
     }
 }
