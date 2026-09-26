050aa062a370fccba67e944b2b95726836318330
HARIPRASAD <152404369+HPMISHRAA@users.noreply.github.com>
2026-09-26 17:53:42 +0530

    Time: 11 ms (6.55%), Space: 174.6 MB (82.68%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,5 +1,6 @@
 class Solution {
     public int[] rearrangeArray(int[] nums) {
+        /*
         int n=nums.length;
         int [] arr=new int[n];
         int pos=0,neg=1;
@@ -14,5 +15,22 @@ class Solution {
         }
         }
         return arr;
+        */
+        ArrayList<Integer> pos = new ArrayList<>();
+        ArrayList<Integer> neg = new ArrayList<>();
+        int n = nums.length;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] < 0) {
+                neg.add(nums[i]);
+            } else {
+                pos.add(nums[i]);
+
+            }
+        }
+        for (int i = 0; i < n / 2; i++) {
+            nums[i * 2] = pos.get(i);
+            nums[i * 2 + 1] = neg.get(i);
+        }
+        return nums;
     }
 }
